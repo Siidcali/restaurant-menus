aa6980a80c0d0b5bed7a614c494128dbad81f473
Siidcali Yusuf <siidaliyusuf7@gmail.com>
2022-08-13 15:32:53 +0100

    added items

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const {Restaurant} = require('./Restaurant')
 const {Menu} = require('./Menu')
+const {Item} = require('./item')
 
-module.exports = { Restaurant, Menu }
+
+Menu.belongsTo(Restaurant)
+Restaurant.hasMany(Menu)
+
+Item.belongsToMany(Menu, {through: 'menu_items'})
+Menu.belongsToMany(Item, {through: 'menu_items'})
+
+
+
+module.exports = { Restaurant, Menu, Item }
